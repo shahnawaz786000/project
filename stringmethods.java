810af3f82797e890f623f5b68e625fe7223895f6
Internship <143068181+shahnawaz786000@users.noreply.github.com>
2023-08-24 20:14:04 +0530

    Add files via upload

---
+++ stringmethods.java
@@ -0,0 +1,15 @@
+public class stringmethods {
+    public static void main(String[] args) {
+       //string into long into string
+       long s = 9999999999l;
+       String i = Long.toString(s);
+       System.out.println(i+333);
+
+    }
+    }
+
+
+
+
+
+ 
